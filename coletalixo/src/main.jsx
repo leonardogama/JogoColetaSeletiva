e3b9b2cfb4052d145084738671f8ee6f20044a93
leonardogama <leo.gama1150@gmail.com>
2025-09-19 09:40:28 -0300

    local funcionando

--- coletalixo/src/main.jsx
+++ coletalixo/src/main.jsx
@@ -1,20 +1,17 @@
-// filepath: c:\projetos\react\JogoColetaSeletiva\coletalixo\src\main.jsx
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { SpeedInsights } from '@vercel/speed-insights/react'; // Importa SpeedInsights
-import Home from './pages/home/Home.jsx';
-import PlayGame from './pages/game/PlayGame.jsx'; // Importa o componente PlayGame
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home.jsx'
+import PlayGame from './pages/game/PlayGame.jsx' // Importa o componente PlayGame
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SpeedInsights>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/play" element={<PlayGame />} /> {/* Rota para o jogo */}
-        </Routes>
-      </BrowserRouter>
-    </SpeedInsights>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/play" element={<PlayGame />} /> {/* Rota para o jogo */}
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-);
+)
